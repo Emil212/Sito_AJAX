b0b0b95c00a396f0ff8570ad1c597e860f659925
Emiliano Ceroncd Waifu-Administratorgit config --global user.name Emiliano <hceronj1400@alumno.ipn.mx>
2021-08-02 05:06:46 -0500

    Add XMLRequest

---
+++ JS/scripts.js
@@ -0,0 +1,46 @@
+"use strinct";
+
+const d = document,
+  $main = d.querySelector("main");
+
+const getHTML = (options) => {
+  /*Options se va a componer de tres partes, url, success y error*/
+  let { url, success, error } = options;
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+      //No se necesita parsear lo que obtengamos de la peticion
+      let html = xhr.responseText;
+      success(html);
+    } else {
+      let message = xhr.statusText || "Ocurrio un error";
+      error(`Error ${xhr.status}: ${message}`);
+    }
+  });
+
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Content-type", "text/html; charset=utf-8");
+  xhr.send();
+};
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  getHTML({
+    url: "ASSETS/home.html",
+    success: (html) => ($main.innerHTML = html),
+    error: (err) => ($main.innerHTML = `<h1>${err}</h1>`),
+  });
+});
+
+d.addEventListener("click", (e) => {
+  if (e.target.matches(".menu a")) {
+    e.preventDefault();
+    getHTML({
+      url: e.target.href,
+      success: (html) => ($main.innerHTML = html),
+      error: (err) => ($main.innerHTML = `<h1>${err}</h1>`),
+    });
+  }
+});
